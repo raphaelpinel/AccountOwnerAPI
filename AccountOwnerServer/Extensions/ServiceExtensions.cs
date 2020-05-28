09be2326d453a11a290a7013aab8e2142d96ee3e
Raphaël Pinel <36964192+raphaelpinel@users.noreply.github.com>
2020-05-28 08:43:10 +0300

    wire servvices extensions and add IIS service

--- AccountOwnerServer/Extensions/ServiceExtensions.cs
+++ AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,5 +19,13 @@ namespace AccountOwnerServer.Extensions
                     .AllowAnyHeader());
             });
         }
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
     }
+
 }
--- AccountOwnerServer/Startup.cs
+++ AccountOwnerServer/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AccountOwnerServer.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,9 @@ namespace AccountOwnerServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
+
             services.AddControllers();
         }
 
